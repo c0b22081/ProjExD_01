969435691999b75de29e2a04f1abd7a18f49f799
c0b22081 <c0b2208126@edu.teu.ac.jp>
2023-09-26 16:28:48 +0900

    練習問題

--- pygame_image.py
+++ pygame_image.py
@@ -7,20 +7,23 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
-    Koukaton_img = pg.image.load("fig/3.png")
-    Koukaton_img = pg.transform.flip(Koukaton_img, True, False)
+    bg_img2 = pg.transform.flip(bg_img,True,False)
+    kk_img = pg.image.load("ex01/fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_img2 = pg.transform.rotozoom(kk_img, 10, 1.0)
+    kk_imgs = [kk_img,kk_img2]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        img_rct = Koukaton_img.get_rect()
-        img_rct.center = 300, 200
-        screen.blit(Koukaton_img, img_rct)
+        x = tmr%1600
+        screen.blit(bg_img, [1600-x, 0])
+        screen.blit(bg_img2, [-x, 0] )
+        screen.blit(kk_imgs[tmr%2], [300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(500)
 
 
 if __name__ == "__main__":
