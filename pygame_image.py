14004a5bfd3ffe9c1208874e1a4181534be18573
c0b22081 <c0b2208126@edu.teu.ac.jp>
2023-09-26 16:27:05 +0900

    練習問題

---
+++ pygame_image.py
@@ -0,0 +1,30 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    Koukaton_img = pg.image.load("fig/3.png")
+    Koukaton_img = pg.transform.flip(Koukaton_img, True, False)
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        img_rct = Koukaton_img.get_rect()
+        img_rct.center = 300, 200
+        screen.blit(Koukaton_img, img_rct)
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
